be5aab4654e9f0669749fbe05e1060e6ff6307ed
Caua Victor Tenorio Santos de Melo <cauameloo@cauas-mbp.wireless-1x.unc.edu>
2024-09-11 15:15:35 -0400

    this is my commitment yay

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,12 @@
+def get_weather_report() -> str:
+    weather: str = input("What is the weather?")
+    if weather == "rainy" or weather == "cold":
+        print("Bring a Jacket")
+    elif weather == "hot":
+        print("Keep cool out there!")
+    else:
+        print("I do not recognize this weather.")
+    return weather
+
+
+print(get_weather_report())
